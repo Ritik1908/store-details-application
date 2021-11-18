bf438f904fa3e88bd603b2f877f258d5d0f22a12
Ritik Verma <ritikverma8100775754@gmail.com>
2021-11-18 18:35:45 +0530

    Jackson Filters Added

--- src/main/kotlin/com/practice/com/storedetails/controller/Controller.kt
+++ src/main/kotlin/com/practice/com/storedetails/controller/Controller.kt
@@ -1,9 +1,12 @@
 package com.practice.com.storedetails.controller
 
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider
 import com.practice.com.storedetails.dao.StoreDetailsJPA
 import com.practice.com.storedetails.exception.NotFoundException
 import com.practice.com.storedetails.model.StoreDetails
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.converter.json.MappingJacksonValue
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
@@ -11,21 +14,32 @@ import java.util.*
 class Controller @Autowired constructor(val storeDetailsJPA: StoreDetailsJPA) {
 
     @GetMapping("/store-service/v1/stores")
-    fun getStores(): MutableList<StoreDetails> {
+    fun getStores(): MappingJacksonValue {
         val data = storeDetailsJPA.findAll()
         if(data.isEmpty()) {
             throw NotFoundException("No records present in database")
         }
-        return data
+        val filter = SimpleBeanPropertyFilter.serializeAllExcept("createdAt")
+        val filtered = SimpleFilterProvider().addFilter("StoreDetailsFilter", filter)
+        var mapping = MappingJacksonValue(data)
+        mapping.filters = filtered
+
+        return mapping
     }
 
     @GetMapping("/store-service/v1/stores/{id}")
-    fun getStore(@PathVariable id: Int): Optional<StoreDetails> {
+    fun getStore(@PathVariable id: Int): MappingJacksonValue {
         val data = storeDetailsJPA.findById(id)
         if(data.isEmpty()) {
             throw NotFoundException("No store found with given id")
         }
-        return data
+
+        val filter = SimpleBeanPropertyFilter.serializeAllExcept("createdAt")
+        val filtered = SimpleFilterProvider().addFilter("StoreDetailsFilter", filter)
+        var mapping = MappingJacksonValue(data)
+        mapping.filters = filtered
+
+        return mapping
     }
 
     @PostMapping("/store-service/v1/stores")
--- src/main/kotlin/com/practice/com/storedetails/model/StoreDetails.kt
+++ src/main/kotlin/com/practice/com/storedetails/model/StoreDetails.kt
@@ -1,5 +1,7 @@
 package com.practice.com.storedetails.model
 
+import com.fasterxml.jackson.annotation.JsonFilter
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.DBRef
@@ -8,12 +10,14 @@ import org.springframework.stereotype.Indexed
 import java.util.*
 
 @Document
+@JsonFilter("StoreDetailsFilter")
 data class StoreDetails(
     @Id
     var id: Int,
     var name: String,
     var status: String,
     var createdAt: Date,
+    @JsonProperty("lastUpdated")
     var updatedAt: Date,
     var addressPeriod: List<AddressPeriod>
 )
