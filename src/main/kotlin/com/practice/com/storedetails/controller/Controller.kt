fbe02e6b0642842ce654de1fdda3bb8a1ff0332a
Ritik Verma <ritikverma8100775754@gmail.com>
2021-11-19 15:41:49 +0530

    Added Query Parameter

--- src/main/kotlin/com/practice/com/storedetails/controller/Controller.kt
+++ src/main/kotlin/com/practice/com/storedetails/controller/Controller.kt
@@ -8,13 +8,26 @@ import com.practice.com.storedetails.model.StoreDetails
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.converter.json.MappingJacksonValue
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 @RestController
 class Controller @Autowired constructor(val storeDetailsJPA: StoreDetailsJPA) {
 
     @GetMapping("/store-service/v1/stores")
-    fun getStores(): MappingJacksonValue {
-        val data = storeDetailsJPA.findAll()
+    fun getStores(@RequestParam(name = "refDate", required = false) date: String?): MappingJacksonValue {
+
+        var data: List<StoreDetails> = mutableListOf()
+
+        if(date != null) {
+            val format = DateTimeFormatter.ofPattern("uuuu-MM-dd", Locale.ENGLISH)
+            val dateConverted = (LocalDate.parse(date.toString(), format))
+            data = storeDetailsJPA.findByUpdatedAtGreaterThan(dateConverted)
+        } else {
+            data = storeDetailsJPA.findAll()
+        }
+
         if(data.isEmpty()) {
             throw NotFoundException("No records present in database")
         }
@@ -29,6 +42,21 @@ class Controller @Autowired constructor(val storeDetailsJPA: StoreDetailsJPA) {
     @GetMapping("/store-service/v1/stores/{id}")
     fun getStore(@PathVariable id: Int): MappingJacksonValue {
         val data = storeDetailsJPA.findById(id)
+        if(data.isEmpty()) {
+            throw NotFoundException("No store found with given id")
+        }
+
+        val filter = SimpleBeanPropertyFilter.serializeAllExcept("createdAt")
+        val filtered = SimpleFilterProvider().addFilter("StoreDetailsFilter", filter)
+        var mapping = MappingJacksonValue(data)
+        mapping.filters = filtered
+
+        return mapping
+    }
+
+    @GetMapping("/store-service/v1/stores/custom")
+    fun getDate(): MappingJacksonValue {
+        val data = storeDetailsJPA.findByUpdatedAtGreaterThan(LocalDate.of(2021,5,5))
         if(data.isEmpty()) {
             throw NotFoundException("No store found with given id")
         }
--- src/main/kotlin/com/practice/com/storedetails/dao/StoreDetailsJPA.kt
+++ src/main/kotlin/com/practice/com/storedetails/dao/StoreDetailsJPA.kt
@@ -2,6 +2,10 @@ package com.practice.com.storedetails.dao
 
 import com.practice.com.storedetails.model.StoreDetails
 import org.springframework.data.mongodb.repository.MongoRepository
+import java.time.LocalDate
 
 interface StoreDetailsJPA: MongoRepository<StoreDetails, Int> {
+
+    fun findByUpdatedAtGreaterThan(date: LocalDate): List<StoreDetails>
+
 }
