018912772e17bb3b069f4e5fc91aed383b82f00f
Ritik Verma <ritikverma8100775754@gmail.com>
2021-11-25 12:59:49 +0530

    Endpoint added as contant

---
+++ src/main/kotlin/com/practice/com/storedetails/configuration/constants.kt
@@ -0,0 +1,6 @@
+package com.practice.com.storedetails.configuration
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+
+const val endpointUrl: String = "/store-service/v1/stores/"
--- src/main/kotlin/com/practice/com/storedetails/controller/Controller.kt
+++ src/main/kotlin/com/practice/com/storedetails/controller/Controller.kt
@@ -1,6 +1,7 @@
 package com.practice.com.storedetails.controller
 
 
+import com.practice.com.storedetails.configuration.endpointUrl
 import com.practice.com.storedetails.model.StoreDetails
 import com.practice.com.storedetails.service.StoreService
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,21 +12,19 @@ import java.util.*
 @RestController
 class Controller (val storeService: StoreService) {
 
-//    @Autowired
-//    @Value("\${store.service.url}")
-//    lateinit var url: String
-
-    @GetMapping("/store-service/v1/stores")
+    @GetMapping(endpointUrl)
     fun getStores(@RequestParam(name = "refDate", required = false) date: String?): List<StoreDetails> {
+//        println(Configuration().endpointUrl)
+//        println(endpointUrl)
         return storeService.getAll(date)
     }
 
-    @GetMapping("/store-service/v1/stores/{id}")
+    @GetMapping(endpointUrl+"{id}")
     fun getStore(@PathVariable id: Int): Optional<StoreDetails> {
         return storeService.getById(id)
     }
 
-    @PostMapping("/store-service/v1/stores")
+    @PostMapping(endpointUrl)
     fun saveStoreDetails(@RequestBody request: StoreDetails): String {
         return storeService.saveStore(request)
     }
